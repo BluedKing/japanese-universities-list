a39842c70b623805869bc3730e7ce1a3f03a11fd
BluedKing <98261059+BluedKing@users.noreply.github.com>
2020-08-19 14:10:54 +0000

    db fix

---
+++ Charting-Infrastructure/Custom-Based-Charts/Custom-Based-Charts.js
@@ -0,0 +1,34 @@
+function createSimpleSwitcher(items, activeItem, activeItemChangedCallback) {
+	var switcherElement = document.createElement('div');
+	switcherElement.classList.add('switcher');
+
+	var intervalElements = items.map(function(item) {
+		var itemEl = document.createElement('button');
+		itemEl.innerText = item;
+		itemEl.classList.add('switcher-item');
+		itemEl.classList.toggle('switcher-active-item', item === activeItem);
+		itemEl.addEventListener('click', function() {
+			onItemClicked(item);
+		});
+		switcherElement.appendChild(itemEl);
+		return itemEl;
+	});
+
+	function onItemClicked(item) {
+		if (item === activeItem) {
+			return;
+		}
+
+		intervalElements.forEach(function(element, index) {
+			element.classList.toggle('switcher-active-item', items[index] === item);
+		});
+
+		activeItem = item;
+
+		activeItemChangedCallback(item);
+	}
+
+	return switcherElement;
+}
+
+var switcherE
